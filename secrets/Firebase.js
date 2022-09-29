ab7f2976b96c0152cace41f38a9c40ec07664b8a
Herimino Rakotondrafara <heriminorakotondrafara@Heriminos-MacBook-Pro.local>
2022-09-29 11:36:52 -0400

    working API call for signup

---
+++ secrets/Firebase.js
@@ -0,0 +1,22 @@
+export class FireBaseSecrets{
+    SignupUrl='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
+    primitiveLink='https://identitytoolkit.googleapis.com/v1/accounts:'
+    API_KEY='AIzaSyDWZcx9JHwZSj2fam2grs4bAL0reJBuIzE'
+    //implement this later    
+    URL=(mode)=>{
+        let modeTranslator;
+        switch(mode){
+            case 'login':
+                modeTranslator='signUp'
+                break
+            case 'signup':
+                modeTranslator='signInWithPassword'
+                break
+        }
+        return this.primitiveLink.concat(modeTranslator,'?key=',this.API_KEY)
+    }
+
+    //readonly SignInUrl:s
+    
+
+}
